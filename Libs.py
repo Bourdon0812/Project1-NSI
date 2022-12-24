c9ebe9179e7e5d290845776f207993602b05433f
-Lucas <lucas.llasbennes@gmail.com>
2022-12-24 17:43:54 +0100

    simplification de hasMinEightChar

--- Libs.py
+++ Libs.py
@@ -22,10 +22,7 @@ def hasMinChar(string: str) -> bool:
 
 # Retourne si une chaine de caracetre a minimum 8 charactere
 def hasMinEightChar(string: str) -> bool:
-    if len(string) >= 8:
-        return True
-    else:
-        return False
+    return len(string) >= 8
 
 
 # Retourne si une chaine de caractere contient au moins 1 caractere special ou un chiffre
